9b5c2e57eec35d245e7d04b33449d085879ddbfb
xifan <xifan233@163.com>
2026-09-03 18:37:47 +0800

    refactor(utils): enhance string_utils const correctness and clean includes

--- src/common/utils/string_utils.h
+++ src/common/utils/string_utils.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <cctype>
 #include <cstdint>
 #include <cstdio>
@@ -104,114 +105,118 @@ inline bool IsSentenceEndingPunctuation(uint32_t cp) {
   return false;
 }
 
-inline size_t Utf8CodepointWidth(uint32_t cp) {
-  if (cp == 0)
+inline size_t Utf8CodepointWidth(uint32_t codepoint) {
+  if (codepoint == 0) {
     return 0;
+  }
   // Control characters & zero-width
-  if (cp < 32 || (cp >= 0x7F && cp < 0xA0) || cp == 0x200B || cp == 0x200C || cp == 0x200D ||
-      cp == 0xFEFF) {
+  if (codepoint < 32 || (codepoint >= 0x7F && codepoint < 0xA0) || codepoint == 0x200B ||
+      codepoint == 0x200C || codepoint == 0x200D || codepoint == 0xFEFF) {
     return 0;
   }
   // CJK, Fullwidth forms, Wide symbols, and Emoji ranges
-  if ((cp >= 0x1100 && cp <= 0x115F) ||   // Hangul Jamo
-      (cp >= 0x2E80 && cp <= 0x9FFF) ||   // CJK Radicals, Punctuation, Ideographs
-      (cp >= 0xAC00 && cp <= 0xD7A3) ||   // Hangul Syllables
-      (cp >= 0xF900 && cp <= 0xFAFF) ||   // CJK Compatibility Ideographs
-      (cp >= 0xFE10 && cp <= 0xFE19) ||   // Vertical forms
-      (cp >= 0xFE30 && cp <= 0xFE6F) ||   // CJK Compatibility Forms
-      (cp >= 0xFF00 && cp <= 0xFF60) ||   // Fullwidth Forms
-      (cp >= 0xFFE0 && cp <= 0xFFE6) ||   // Fullwidth Symbols
-      (cp >= 0x1F000 && cp <= 0x1FAFF) || // Miscellaneous Symbols, Pictographs & Emojis
-      (cp >= 0x20000 && cp <= 0x2FA1F) || // CJK Unified Ideographs Extension
-      (cp >= 0x30000 && cp <= 0x3134F)) {
+  if ((codepoint >= 0x1100 && codepoint <= 0x115F) ||   // Hangul Jamo
+      (codepoint >= 0x2E80 && codepoint <= 0x9FFF) ||   // CJK Radicals, Punctuation, Ideographs
+      (codepoint >= 0xAC00 && codepoint <= 0xD7A3) ||   // Hangul Syllables
+      (codepoint >= 0xF900 && codepoint <= 0xFAFF) ||   // CJK Compatibility Ideographs
+      (codepoint >= 0xFE10 && codepoint <= 0xFE19) ||   // Vertical forms
+      (codepoint >= 0xFE30 && codepoint <= 0xFE6F) ||   // CJK Compatibility Forms
+      (codepoint >= 0xFF00 && codepoint <= 0xFF60) ||   // Fullwidth Forms
+      (codepoint >= 0xFFE0 && codepoint <= 0xFFE6) ||   // Fullwidth Symbols
+      (codepoint >= 0x1F000 && codepoint <= 0x1FAFF) || // Miscellaneous Symbols & Emojis
+      (codepoint >= 0x20000 && codepoint <= 0x2FA1F) || // CJK Unified Ideographs Extension
+      (codepoint >= 0x30000 && codepoint <= 0x3134F)) {
     return 2;
   }
   return 1;
 }
 
-inline uint32_t NextUtf8Codepoint(std::string_view s, size_t& offset) {
-  if (offset >= s.size())
+inline uint32_t NextUtf8Codepoint(std::string_view text, size_t& offset) {
+  if (offset >= text.size()) {
     return 0;
-  auto c = static_cast<unsigned char>(s[offset]);
-  uint32_t cp = 0;
+  }
+  const auto c = static_cast<unsigned char>(text[offset]);
+  uint32_t codepoint = 0;
   size_t len = 1;
   if (c < 0x80) {
-    cp = c;
+    codepoint = c;
     len = 1;
   } else if ((c & 0xE0) == 0xC0) {
-    cp = c & 0x1F;
+    codepoint = c & 0x1F;
     len = 2;
   } else if ((c & 0xF0) == 0xE0) {
-    cp = c & 0x0F;
+    codepoint = c & 0x0F;
     len = 3;
   } else if ((c & 0xF8) == 0xF0) {
-    cp = c & 0x07;
+    codepoint = c & 0x07;
     len = 4;
   } else {
     offset += 1;
     return c;
   }
-  for (size_t i = 1; i < len && offset + i < s.size(); ++i) {
-    cp = (cp << 6) | (static_cast<unsigned char>(s[offset + i]) & 0x3F);
+  for (size_t i = 1; i < len && offset + i < text.size(); ++i) {
+    codepoint = (codepoint << 6) | (static_cast<unsigned char>(text[offset + i]) & 0x3F);
   }
   offset += len;
-  return cp;
+  return codepoint;
 }
 
-inline size_t Utf8VisualWidth(std::string_view s) {
+inline size_t Utf8VisualWidth(std::string_view text) {
   size_t width = 0;
   size_t offset = 0;
-  while (offset < s.size()) {
-    uint32_t cp = NextUtf8Codepoint(s, offset);
-    width += Utf8CodepointWidth(cp);
+  while (offset < text.size()) {
+    const uint32_t codepoint = NextUtf8Codepoint(text, offset);
+    width += Utf8CodepointWidth(codepoint);
   }
   return width;
 }
 
 inline std::string TruncateMiddleUtf8(std::string_view text, size_t max_width,
                                       std::string_view ellipsis = "...") {
-  if (max_width == 0)
+  if (max_width == 0) {
     return std::string(text);
+  }
 
-  size_t total_width = Utf8VisualWidth(text);
-  if (total_width <= max_width)
+  const size_t total_width = Utf8VisualWidth(text);
+  if (total_width <= max_width) {
     return std::string(text);
+  }
 
-  size_t ellipsis_width = Utf8VisualWidth(ellipsis);
+  const size_t ellipsis_width = Utf8VisualWidth(ellipsis);
   if (max_width <= ellipsis_width + 4) {
-    size_t tail_budget = max_width;
-    size_t i = text.size();
+    const size_t tail_budget = max_width;
+    size_t index = text.size();
     size_t current_width = 0;
-    while (i > 0) {
-      size_t prev_i = i - 1;
-      while (prev_i > 0 && (static_cast<unsigned char>(text[prev_i]) & 0xC0) == 0x80) {
-        --prev_i;
+    while (index > 0) {
+      size_t prev_index = index - 1;
+      while (prev_index > 0 && (static_cast<unsigned char>(text[prev_index]) & 0xC0) == 0x80) {
+        --prev_index;
       }
-      uint32_t cp = FirstUtf8Codepoint(text.substr(prev_i));
-      size_t cp_width = Utf8CodepointWidth(cp);
+      const uint32_t codepoint = FirstUtf8Codepoint(text.substr(prev_index));
+      const size_t cp_width = Utf8CodepointWidth(codepoint);
       if (current_width + cp_width > tail_budget) {
         break;
       }
       current_width += cp_width;
-      i = prev_i;
+      index = prev_index;
     }
-    return std::string(text.substr(i));
+    return std::string(text.substr(index));
   }
 
-  size_t available_content_width = max_width - ellipsis_width;
+  const size_t available_content_width = max_width - ellipsis_width;
   size_t target_head_width = std::max<size_t>(6, std::min<size_t>(14, available_content_width / 4));
   if (target_head_width >= available_content_width) {
     target_head_width = available_content_width / 2;
   }
-  size_t target_tail_width = available_content_width - target_head_width;
+  const size_t target_tail_width = available_content_width - target_head_width;
 
   size_t head_byte_len = 0;
   size_t head_cur_width = 0;
   size_t offset = 0;
   while (offset < text.size()) {
     size_t next_offset = offset;
-    uint32_t cp = NextUtf8Codepoint(text, next_offset);
-    size_t cp_width = Utf8CodepointWidth(cp);
+    const uint32_t codepoint = NextUtf8Codepoint(text, next_offset);
+    const size_t cp_width = Utf8CodepointWidth(codepoint);
     if (head_cur_width + cp_width > target_head_width) {
       break;
     }
@@ -222,20 +227,20 @@ inline std::string TruncateMiddleUtf8(std::string_view text, size_t max_width,
 
   size_t tail_start_offset = text.size();
   size_t tail_cur_width = 0;
-  size_t i = text.size();
-  while (i > head_byte_len) {
-    size_t prev_i = i - 1;
-    while (prev_i > 0 && (static_cast<unsigned char>(text[prev_i]) & 0xC0) == 0x80) {
-      --prev_i;
+  size_t index = text.size();
+  while (index > head_byte_len) {
+    size_t prev_index = index - 1;
+    while (prev_index > 0 && (static_cast<unsigned char>(text[prev_index]) & 0xC0) == 0x80) {
+      --prev_index;
     }
-    uint32_t cp = FirstUtf8Codepoint(text.substr(prev_i));
-    size_t cp_width = Utf8CodepointWidth(cp);
+    const uint32_t codepoint = FirstUtf8Codepoint(text.substr(prev_index));
+    const size_t cp_width = Utf8CodepointWidth(codepoint);
     if (tail_cur_width + cp_width > target_tail_width) {
       break;
     }
     tail_cur_width += cp_width;
-    i = prev_i;
-    tail_start_offset = i;
+    index = prev_index;
+    tail_start_offset = index;
   }
 
   std::string result;
