48d3c7b56433c143074c64c7fd4b2439e98e41d7
xifan <xifan233@163.com>
2026-09-08 00:32:47 +0800

    chore(ci): clean up test friend in vinput.h and drop test step in ci workflow (#215)

--- src/addon/core/vinput.h
+++ src/addon/core/vinput.h
@@ -32,11 +32,8 @@
 
 class VinputNotifierDBusObject;
 
-class VinputTestAccessor;
-
 class VinputEngine : public fcitx::AddonInstance {
 public:
-  friend class VinputTestAccessor;
   VinputEngine(fcitx::Instance* instance);
   ~VinputEngine() override;
   void selectPaletteItem(std::size_t index, fcitx::InputContext* ic);
